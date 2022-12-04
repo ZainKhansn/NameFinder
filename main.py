c575aa519242e99de78662956bc9d77e55c697c8
Zain Khan <92826966+ZainKhansn@users.noreply.github.com>
2022-12-04 14:01:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 # importing pandas as pd
+#Using Replit to clear console
 import time
 import replit
 import pandas as pd
  
-# importing re for regular expressions
+
 na = input("Enter a name!: ")
 replit.clear()
 # Creating the Series
 df = pd.read_csv("names.csv")
+#Opening csv file
 names = df.loc[df['name'].str.contains(na, case=False)]
+#checking case for names
 
 i = 0
 for name in range(len(names)):
   print(names)
   time.sleep(3)
 replit.clear()
+#loops through csv file to find name
 
 quit()
+#exit
 
 
 
